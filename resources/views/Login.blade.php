3fcb184a87f42d633b2916339e080f0abc3412d0
ASUS-NOTEBOOK\WILLIAM <william.onggo@gmail.com>
2017-05-29 18:26:12 +0700

    Regiter Login

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,25 @@
+!DOCTYPE html>
+<html>
+<head>
+	<title>Parkiran Yusi</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+		<div class="row">
+			<div class="col-sm-8">
+			<h2>Parkiran Yusi</h2>
+			<h3>UserName : </h3>
+			<input type="type" class="form-control" name="username" placeholder="Input UserName" >
+			<h3>Password : </h3>
+			<input type="type" class="form-control" name="password" placeholder="Input Password" >
+			</div>
+			<button type="button" class="btn btn-link">Forgot the password ?</button>
+			<button type="button" class="btn btn-primary">Login</button>
+			</div>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
